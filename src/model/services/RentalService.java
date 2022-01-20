0af3975029a1590b43bd4422df5795c693da23fb
MatheusDev22 <matheusbsb95@gmail.com>
2022-01-20 03:19:23 -0300

    Create Projetc

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,24 @@
+package model.services;
+
+import model.entityes.CarRental;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+	
+	public RentalService() {
+	}
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		super();
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+	}
+}
